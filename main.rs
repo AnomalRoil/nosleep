ec08b1447e57b61abc77f311a9cf8a3891cd2a24
Yolan Romailler <yromailler@fb.com>
2021-02-23 18:13:27 +0100

    Tidying code

--- main.rs
+++ main.rs
@@ -1,7 +1,6 @@
 use enigo::{Enigo, Key, KeyboardControllable};
 use std::thread;
 use std::time::Duration;
-use chrono;
 
 fn main() {
     let mut enigo = Enigo::new();
@@ -9,9 +8,12 @@ fn main() {
     println!("Hello World! Let's avoid sleep for a while... ❤️ ");
     println!("To quit, use ctrl-C.");
     loop {
-	// We sleep for 2 minutes
+        // We sleep for 2 minutes
         thread::sleep(Duration::from_secs(120));
         enigo.key_up(Key::Raw(0x69));
-	println!("Pressed F13 at {}", chrono::offset::Utc::now().format("%Y-%m-%d @ %H:%M:%S"));
+        println!(
+            "Pressed F13 at {}",
+            chrono::offset::Utc::now().format("%Y-%m-%d @ %H:%M:%S")
+        );
     }
 }
